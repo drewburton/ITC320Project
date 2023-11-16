37e9b6d6fc607b9710200bfe755a32c219fe71ff
drewburton <drew.burton.gc@gmail.com>
2023-11-16 10:29:38 -0500

    added common css file for header and started calendar

---
+++ calendar.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(() => {
+	$("#home").click(() => location.href = "index.html");
+	$("#calendar").click(() => location.reload());
+	$("#events").click(() => location.href = "config.html");
+	$("#about").click(() => location.href = "about.html");
+});
