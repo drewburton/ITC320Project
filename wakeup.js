397085f94d3f326a8c5acb0448cf8322d619a21b
drewburton <drew.burton.gc@gmail.com>
2023-11-27 12:10:10 -0500

    added css and function in js to perform animation of gradient for alarm

--- wakeup.js
+++ wakeup.js
@@ -1,9 +1,16 @@
 "use strict";
 
+
+function performAnimation() {
+	$('body').css('animation', 'Animation 5s ease forwards');
+}
+
 $(document).ready(() => {
 	$("#home").click(() => location.href = "index.html");
 	$("#wakeup").click(() => location.reload());
 	$("#calendar").click(() => location.href = "calendar.html");
 	$("#events").click(() => location.href = "config.html");
 	$("#about").click(() => location.href = "about.html");
+
+	performAnimation();
 });
